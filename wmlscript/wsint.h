32ae62b858a044d98272c0d2a651b8bc93f3d895
Norbert Kwizera <norkans7@gmail.com>
2026-05-08 13:47:55 +0200

    Fix build on ubuntu 26.04

--- wmlscript/wsint.h
+++ wmlscript/wsint.h
@@ -273,7 +273,7 @@ typedef struct WsCompilerRec WsCompiler;
 #define YYLEX_PARAM     pctx
 
 /* The lexer. */
-extern int yylex(int, ...);
+extern int yylex(YYSTYPE *lvalp, void *ctx);
 
 /* Register the lexer allocated block `ptr' to the compiler's list of
    active blocks. */
